7a1a84fb971c08c2c7a6b521d4124c5636904393
Kostiantyn Osypenko <102372895+Osipec@users.noreply.github.com>
2022-11-27 17:35:18 +0200

    Update Homework#10_Osypenko.py

--- Homework#10_Osypenko.py
+++ Homework#10_Osypenko.py
@@ -1,62 +1,70 @@
 # Lesson 9, Homework 10, Osypenko Kostiantyn
 
 # 1. Sequence finder
-# import math
-#
-# def seq_find(sequence: str):
-#     new_seq = tuple(map(int, sequence.replace(',', ' ').split()))
-#     # print(new_seq)
-#     c = new_seq[-1]
-#     b = new_seq[-2]
-#     a = new_seq[-3]
-#     res = seq_1(a, b, c) or seq_2(a, b, c) or seq_3(a, b, c) or \
-#           seq_4(a, b, c) or seq_5(a, b, c) or seq_6(a, b, c)
-#     return res
-# '''Functions below discribe sequence and returns next element. If you want to add a new sequence
-# write a function and add it's call in line 13. Parameters a, b, c are three last elements of sequence,
-# d is the element to find. New function should return d if a, b, c satisfy regularity of sequence, if not, return False'''
-# def seq_1(a: int, b: int, c: int):
-#     if a + 2 == b and b + 2 == c:
-#         d = c + 2
-#         return d
-#     else:
-#         return False
-#
-# def seq_2(a: int, b: int, c: int):
-#     if a + 3 == b and b + 2 == c:
-#         d = c + 3
-#         return d
-#     else:
-#         return False
-#
-# def seq_3(a: int, b: int, c: int):
-#     if a * 2 == b and b * 2 == c:
-#         d = c * 2
-#         return d
-#     else:
-#         return False
-#
-# def seq_4(a: int, b: int, c: int):
-#     if a * 3 == b and b * 3 == c:
-#         d = c * 3
-#         return d
-#     else:
-#         return False
-#
-# def seq_5(a: int, b: int, c: int):
-#     if (c - b) - (b - a) == 2:
-#         d = c + (c - b) + 2
-#         return d
-#
-# def seq_6(a: int, b: int, c: int):
-#     if math.ceil(c ** (1 / 3)) - math.ceil(b ** (1 / 3)) == 1 and math.ceil(b ** (1 / 3)) - math.ceil(a ** (1 / 3)) == 1:
-#         d = (math.ceil(c ** (1 / 3)) + 1) ** 3
-#         return d
-#     else:
-#         return False
-#
-# print(seq_find(input('Enter sequence to find next element:\n')) or 'No sequence in data base')
+import math
+
+
+def seq_find(sequence: str, func):
+    new_seq = tuple(map(int, sequence.replace(',', ' ').split()))
+    return func(new_seq)
+
+
+def seq_1_plus_2(seq):
+    for i in range(len(seq) - 1):
+        if seq[i + 1] != seq[i] + 2:
+            return "Sequence Error"
+    else:
+        return seq[-1] + 2
+
+
+def seq_2_plus_3(seq):
+    for i in range(len(seq) - 1):
+        if seq[i + 1] != seq[i] + 3:
+            return "Sequence Error"
+    else:
+        return seq[-1] + 3
+
+
+def seq_3_multy_2(seq):
+    for i in range(len(seq) - 1):
+        if seq[i + 1] != seq[i] * 2:
+            return "Sequence Error"
+    else:
+        return seq[-1] * 2
+
+
+def seq_4_multy_3(seq):
+    for i in range(len(seq) - 1):
+        if seq[i + 1] != seq[i] * 3:
+            return "Sequence Error"
+    else:
+        return seq[-1] * 3
+
+
+def seq_5_plus_odd(seq):
+    for i in range(len(seq) - 1):
+        if not (seq[i + 1] - seq[i]) % 2:
+            return "Sequence Error"
+    else:
+        return seq[-1] + (seq[-1] - seq[-2] + 2)
+
+
+def seq_6_pow_3(seq):
+    for i in range(len(seq) - 1):
+        if math.ceil(seq[i + 1] ** (1 / 3)) - math.ceil(seq[i] ** (1 / 3)) != 1:
+            return "Sequence Error"
+    else:
+        return (math.ceil(seq[-1] ** (1 / 3)) + 1) ** 3
+
+
+input_seq = input('Enter sequence: ')
+print(seq_find(input_seq, seq_1_plus_2))
 
 # 2. Palindrome
-# pali = max(set(((a * b), a, b) for a in range(9000, 10000) for b in range(9000, 10000) if str(a*b) == str(a*b)[::-1]))
+# import time
+# start = time.time()
+# pali = max(list(((a * b), a, b) for a in range(100_000, 99_000, -1) for b in range(100_000, 99_000, -1)
+#                 if str(a * b) == str(a * b)[::-1]))
 # print(f'Max palindrome is {pali[0]}, a = {pali[1]}, b = {pali[2]}')
+# end = time.time() - start
+# print(end)
